d8a0a2aca1ac859cacbcc16a4d1ffab6bfb50e31
zeyang <zeyang@BIH-D-2890.netease.internal>
2014-08-04 18:57:41 +0800

    改正错误

--- main/models.py
+++ main/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-from test.test_imageop import MAX_LEN
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
