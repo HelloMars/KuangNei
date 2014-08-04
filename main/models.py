8034224176332ef5f94fcd5a6b21fbc4319350ad
zeyang <zeyang@BIH-D-2890.netease.internal>
2014-08-04 17:49:21 +0800

    创建信的MODEL

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
+from test.test_imageop import MAX_LEN
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,3 +13,20 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+class Post(models.Model):
+    user_id = models.BigIntegerField()
+    school_id = models.IntegerField()
+    content = models.CharField(max_length=800)
+    channel = models.IntegerField()
+    unlike_count = models.IntegerField()
+    create_time = models.DateTimeField('date published')  
+    back_count = models.IntegerField()
+    current_floor = models.IntegerField()
+    rank = models.IntegerField() 
+    
+class Post_picture(models.Model):
+    post_id = models.BigIntegerField()
+    picture_url = models.URLField()
+    create_time = models.DateTimeField()
+    picture_size = models.CharField()
