cd529278a7a0415293179767b835c879a4e99b72
ndsun <feidaowangzixing@163.com>
2014-08-05 22:36:17 +0800

    阿扒村

--- kuangnei/__init__.py
+++ kuangnei/__init__.py
@@ -1,1 +1,7 @@
+import qiniu.conf
 print "@@@@@@@@@@@"
+
+
+print "^^^^^^^^^^^^^^^^^^^^^^^"
+qiniu.conf.ACCESS_KEY = "qZUvN3pdML7x0pa4LPoP2iLI5iif0DP1l5JLx1Ax"
+qiniu.conf.SECRET_KEY = "oPFZOqG6dV2cuX1krZsLT1vkDBVeEzaUKYZYuQsc"
