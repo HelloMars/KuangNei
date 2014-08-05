a7bdf1f9f1cfe4ea990db91befdb5c4490e0e96d
ndsun <feidaowangzixing@163.com>
2014-08-05 22:26:52 +0800

    大幅度v

---
+++ main/__init__.py
@@ -0,0 +1,5 @@
+import qiniu.conf
+
+print "^^^^^^^^^^^^^^^^^^^^^^^"
+qiniu.conf.ACCESS_KEY = "qZUvN3pdML7x0pa4LPoP2iLI5iif0DP1l5JLx1Ax"
+qiniu.conf.SECRET_KEY = "oPFZOqG6dV2cuX1krZsLT1vkDBVeEzaUKYZYuQsc"
