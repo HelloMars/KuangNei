4bca6853015006759ce709cae4c5b230e4acfe58
zeyang <zeyang@BIH-D-2890.netease.internal>
2014-08-18 15:17:55 +0800

    a

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,10 @@ urlpatterns = patterns(
     url(r'^channellist/$', views.channellist, name='channellist'),
     url(r'^postlist/$', views.postlist, name='postlist'),
     url(r'^getUpToken/$', views.get_uptoken, name='getUpToken'),
-    url(r'^getDnUrl/$', views.get_dnurl, name='getDnUrl')
+    url(r'^getDnUrl/$', views.get_dnurl, name='getDnUrl'),
+    url(r'^checkIfUserExist/$', views.check_if_user_exist, name='checkIfExist'),
+    url(r'^signin/$', views.login_in, name='login'),
+    url(r'^logout/$', views.logout_out, name='logout'),
+    url(r'^testLogin/$', views.test_view, name='testLogin'),
+    url(r'^register/$', views.register, name='regist')
 )
