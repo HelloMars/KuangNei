0d51914623b8a64fa194ef6fedb79cb2b6bcf9c3
ndsun <feidaowangzixing@163.com>
2014-08-21 10:34:30 +0800

    a

--- main/views.py
+++ main/views.py
@@ -97,9 +97,10 @@ def register(request):
                     ret = utils.wrap_message(code=10, msg='注册失败')
                     logger.warn('注册失败')
                 else:
+                    UserInfo.objects.create(userId=newuser.id, token=token)  #在user_info表中设置token
                     user = authenticate(username=username, password=password)
                     login(request, user)
-                    request.session.set_expiry(300)
+                    request.session.set_expiry(300)     #session失效期5分钟
                     ret = utils.wrap_message({'user': newuser.username})
                     logger.info('注册新用户(%s)成功', repr(newuser))
             else:
